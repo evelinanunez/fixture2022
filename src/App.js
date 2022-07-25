ecb542e9f0b28b52c9a06cb65cc11584def438cd
evelinanunez <evelinacelestenunez@gmail.com>
2022-07-25 20:33:03 -0300

    Se crea componente Home y se reorganiza componentes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from "react"; // Esto es porque uso la librearia React en esta app.
 import "./App.css";
-import Nav from "./components/Home/Nav";
-import UltimasNoticias from "./components/Home/UltimasNoticias";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
-      <Nav />
-      <main>
-        <div className="container mt-5">
-          <div className="row">
-                        <UltimasNoticias />
-          </div>
-        </div>
-      </main>
+      <Home/>
     </>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
+import Nav from "./Nav";
+import PartidosHoy from "./PartidosHoy";
+import UltimasNoticias from "./UltimasNoticias";
 
-/*
 const Home = () =>{
     return (
-
+        <>
+        <Nav/>
+        <main>
+        <div className="container mt-5">
+          <div className="row">
+                        <PartidosHoy/>
+                        <UltimasNoticias/>
+          </div>
+        </div>
+      </main>
+        </>
     );
 }
 
 export default Home;
-*/
