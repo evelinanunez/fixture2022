b7f4744865a7b49a6660355d046db581e718cabb
evelinanunez <evelinacelestenunez@gmail.com>
2022-08-06 00:02:17 -0300

    se modifica componente TablePartidos y partidos

--- src/components/FaseDeGrupos/Partido.js
+++ src/components/FaseDeGrupos/Partido.js
@@ -11,37 +11,14 @@ const Partido = (props) => {
     minute: "2-digit",
   });
   return (
-    <>
-      {/**
-       * tr: row
-       * td:data
-       * th:head
-       */}
-      <div className="col-12">
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>Grupo</th>
-              <th>Fecha</th>
-              <th>Equipo 1</th>
-              <th>Equipo 2</th>
-              <th>Resultados</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr key={props.id}>
-              <td>{props.grupo}</td>
-              <td>{fechaLocal + " - " + horaLocal}</td>
-              <td>{props.equipo1}</td>
-              <td>{props.equipo2}</td>
-              <td>{props.resultado1}</td>
-              <td>{props.resultado2}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </>
+    <tr key={props.id}>
+      <td>{props.grupo}</td>
+      <td>{fechaLocal + " - " + horaLocal}</td>
+      <td>{props.equipo1}</td>
+      <td>{props.equipo2}</td>
+      <td>{props.resultado1}</td>
+      <td>{props.resultado2}</td>
+    </tr>
   );
 };
 
--- src/components/FaseDeGrupos/TablePartidos.js
+++ src/components/FaseDeGrupos/TablePartidos.js
@@ -3,11 +3,27 @@ import informacion from "../../assets/Informacion.json";
 import Partido from "./Partido";
 
 const TablePartidos = () => {
-  const { Totalpartidos } = informacion;
-  Totalpartidos.map((partido) => {
+  const { partidos } = informacion;
+  partidos.map((partido) => {
     return (
       <div className="row">
-        <Partido TotalPartidos={partido} />
+        <div className="col-12">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Grupo</th>
+                <th>Fecha</th>
+                <th>Equipo 1</th>
+                <th>Equipo 2</th>
+                <th>Resultados</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Partido DatosPartido={partido} />
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   });
