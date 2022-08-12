36c01e98b60245e0950638b109c398a0d1679e65
Matias Alarcon <matiasalarcon55@hotmail.com>
2022-08-12 00:31:16 -0300

    Correccion de fase de grupos

--- src/components/FaseDeGrupos/Partido.js
+++ src/components/FaseDeGrupos/Partido.js
@@ -1,25 +1,30 @@
 import React from "react";
 
 const Partido = (props) => {
-  const fecha1 = new Date(props.fecha);
-  const fechaLocal = fecha1.toLocaleDateString("es-AR", {
-    month: "long",
-    day: "numeric",
-  });
-  const horaLocal = fecha1.toLocaleTimeString("es-AR", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-  return (
-    <tr key={props.id}>
-      <td>{props.grupo}</td>
-      <td>{fechaLocal + " - " + horaLocal}</td>
-      <td>{props.equipo1}</td>
-      <td>{props.equipo2}</td>
-      <td>{props.resultado1}</td>
-      <td>{props.resultado2}</td>
-    </tr>
-  );
+	const { datosPartido } = props;
+	const { fecha, id, grupo, equipo1, equipo2, resultado1, resultado2 } =
+		datosPartido;
+	const fecha1 = new Date(fecha);
+	const fechaLocal = fecha1.toLocaleDateString("es-AR", {
+		month: "long",
+		day: "numeric",
+	});
+	const horaLocal = fecha1.toLocaleTimeString("es-AR", {
+		hour: "2-digit",
+		minute: "2-digit",
+	});
+	return (
+		<tr key={id}>
+			<td>{grupo}</td>
+			<td>{fechaLocal + " - " + horaLocal}</td>
+			<td>{equipo1}</td>
+			<td>{equipo2}</td>
+			<td>
+				{resultado1 && resultado2 ? resultado1 + " a " + resultado2 : "-"}
+			</td>
+			<td>Botones</td>
+		</tr>
+	);
 };
 
 export default Partido;
--- src/components/FaseDeGrupos/TablePartidos.js
+++ src/components/FaseDeGrupos/TablePartidos.js
@@ -3,30 +3,31 @@ import informacion from "../../assets/Informacion.json";
 import Partido from "./Partido";
 
 const TablePartidos = () => {
-  const { partidos } = informacion;
-  partidos.map((partido) => {
-    return (
-      <div className="row">
-        <div className="col-12">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th>Grupo</th>
-                <th>Fecha</th>
-                <th>Equipo 1</th>
-                <th>Equipo 2</th>
-                <th>Resultados</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              <Partido DatosPartido={partido} />
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-  });
+	const { partidos } = informacion;
+
+	return (
+		<div className='row'>
+			<div className='col-12'>
+				<table className='table table-hover'>
+					<thead>
+						<tr>
+							<th>Grupo</th>
+							<th>Fecha</th>
+							<th>Equipo 1</th>
+							<th>Equipo 2</th>
+							<th>Resultados</th>
+							<th>Acciones</th>
+						</tr>
+					</thead>
+					<tbody>
+						{partidos.map((partido) => {
+							return <Partido datosPartido={partido} />;
+						})}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 };
 
 export default TablePartidos;
