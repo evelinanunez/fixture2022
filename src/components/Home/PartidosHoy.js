d29c9dfd2d4fdff324b0d36f546ab7bf76e6eb69
Matias Alarcon <matiasalarcon55@hotmail.com>
2022-09-08 23:33:26 -0300

    Corecciones 08-09

--- src/components/Home/PartidosHoy.js
+++ src/components/Home/PartidosHoy.js
@@ -2,11 +2,11 @@ import React from "react";
 import TableHome from "./TableHome";
 
 const PartidosHoy = () => {
-  return (
-    <section className="col-12 col-lg-6">
-      <TableHome titulo="Partidos Hoy" />;
-    </section>
-  );
+	return (
+		<section className='col-12 col-lg-6'>
+			<TableHome titulo='Partidos Hoy' />
+		</section>
+	);
 };
 
 export default PartidosHoy;
--- src/components/OctavosDeFinal/NuevoPartido.js
+++ src/components/OctavosDeFinal/NuevoPartido.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 const NuevoPartido = () => {
-  return (
-    <div classNameName="row my-2 ms-1">
-      <button
-        type="button"
-        className="btn btn-dark col-2 p-0"
-        data-bs-toggle="modal"
-      >
-        Nuevo Partido
-      </button>
-    </div>
-  );
+	return (
+		<div classNameName='row my-3'>
+			<button
+				type='button'
+				className='btn btn-dark col-2'
+				data-bs-toggle='modal'>
+				Nuevo Partido
+			</button>
+		</div>
+	);
 };
 
 export default NuevoPartido;
